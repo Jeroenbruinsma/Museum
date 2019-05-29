2811c2b1c12812335e29f7f7e0b9fe75700da577
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-05-29 11:30:21 +0200

    Made a handler for empty input

--- museum-app/pages/detail-page.js
+++ museum-app/pages/detail-page.js
@@ -1,15 +1,23 @@
 function submitComment() {
     const inputField = document.getElementById('name')
-    const name = inputField.value
+    let name = inputField.value
     const textArea = document.getElementById('msg')
-    const msg = textArea.value
+    let msg = textArea.value
     console.log("msg: " + msg)
     console.log("name: " + name)
     const comment = document.createElement('section')
-    const h3 =  document.createElement('h3')
-    const p =  document.createElement('p')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+    if (name == "") {
+        console.log("Name was empty")
+        name = "Anonymous"
+    }
+    if (msg == "") {
+        console.log("Msg was empty")
+        msg = name + " Forgot to enter a message"
+    }
 
-    h3.innerHTML = `${name} said:`; 
+    h3.innerHTML = `${name} said:`;
     p.innerHTML = `${msg}`;
     comment.classList.add('comment')
     comment.appendChild(h3)
