908eb151521ee1ef46a5652cd489d7357d6579c4
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-05-30 16:16:04 +0200

    css index + detail page is dynamic

--- museum-app/proccesData.js
+++ museum-app/proccesData.js
@@ -3,7 +3,7 @@ paintings = data.artObjects;
 console.log("Amount of paintings: " + paintings.length)
 for (var i = 0; i < paintings.length; i++) {
     //console.log(paintings[i]);
-    makeElement(paintings[i])
+    makeElement(paintings[i],i)
     
     //gallery.appendChild(a)
     //gallery.appendChild(img)
@@ -12,7 +12,7 @@ for (var i = 0; i < paintings.length; i++) {
 
 
 
-function makeElement(data) {
+function makeElement(data,id) {
 
     let a = document.createElement('a')
     let img = document.createElement('img')
@@ -21,6 +21,8 @@ function makeElement(data) {
     img.alt = data.title;
     img.class = "artObject";
     a.href = "./pages/detail-page.html"
+    a.href += `?id=${id}`;
+    
 
     a.appendChild(img)
 
